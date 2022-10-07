5ee8fc5667e8a8f564a0446c3d73b4f19e45af2b
Said-Asis <102001506+Said-Asis@users.noreply.github.com>
2022-10-06 21:46:44 -0300

    Add files via upload

---
+++ Tarea JS/script.js
@@ -0,0 +1,44 @@
+//#region consolas
+let nombre=`Variable`
+console.log(nombre)
+var nombre2=`Variable 2`
+console.log(nombre2)
+const nombre3=`Variable 3`
+console.log(nombre3)
+//#endregion consolas
+
+//#region funciones
+let suma=FuncionSuma ()
+
+function FuncionSuma() {
+    let resultado= 3+5
+    return resultado
+}
+
+let resta=FuncionResta ()
+
+function FuncionResta() {
+    let resultado= 5-3
+    return resultado
+}
+
+let mult=FuncionMult ()
+
+function FuncionMult() {
+    let resultado= 2*5
+    return resultado
+}
+
+let div=FuncionDiv ()
+
+function FuncionDiv() {
+    let resultado= 8/4
+    return resultado
+}
+//#endregion funciones
+
+//#region arreglo
+var arrayEJ=[1,2,3]
+
+for (inicio of arrayEJ);
+//#endregion arreglo
